b87193b0436e4ea9138d93f52154c953a50070b0
rauelen <129184726+rauelen@users.noreply.github.com>
2024-05-02 14:28:56 -0300

    bdfhgdfthg

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// Função para mostrar o pop-up de boas-vindas
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(mostrarPopup, 2000);
+});
+
 function mostrarPopup() {
     document.getElementById('popup').style.display = 'block';
 }
 
-// Função para fechar o pop-up
 function fecharPopup() {
     document.getElementById('popup').style.display = 'none';
 }
-
-// Chamar a função para mostrar o pop-up após um atraso de 2 segundos (2000 milissegundos)
-setTimeout(mostrarPopup, 2000);
